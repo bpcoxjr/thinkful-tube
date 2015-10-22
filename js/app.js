fbbe98b39ad9af1f4bf31db4eb67a3ee6c64e663
Bruce P Cox Jr <bpcoxjr@yahoo.com>
2015-10-22 09:36:26 -0400

    updated js

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	console.log("ready");
 	$('#search-term').submit(function(event){
 		event.preventDefault();
-		console.log("submit");
+		console.log("submit clicked");
 		var searchTerm = $('#query').val();
 		getRequest(searchTerm);
 	});
@@ -10,6 +10,7 @@ $(document).ready(function(){
 
 
 function getRequest(searchTerm){
+	//set up a params variable to pass to .getJSON that meets the Youtube API requirements
 	var params = {
 		part: 'snippet',
 		q: searchTerm,
@@ -25,13 +26,26 @@ function getRequest(searchTerm){
 
 function showResults(results){
 	var videoList ="";
+	var ul = $('<ul>');
+	$('#search-results').html(ul);
 	$.each(results, function(key, value){
 		var thumb = value.snippet.thumbnails.medium.url;
 		var title = value.snippet.title;
         var videoId = value.id.videoId;
         var youtubeSearch = "https://www.youtube.com/watch?v=";
         console.log(thumb);
-        videoList += '<ul><li><p>' + title + '</p><img src="' + thumb + ' "></li></ul>';
-          $('#search-results').prepend(videoList).wrap('<a href=' + youtubeSearch + videoId + '></a>');
+        //create list item and place it in a variable called li
+        var li = $("<li>");
+        //create title
+        var title = $('<p>' + title + '</p>');
+        //create image tag and set src equal to var thumb
+        var img = $('<img>').attr('src', thumb);
+        //add title and img to list item
+        $(li).append([title, img]);
+        //wrap title and img in a link
+        title.wrap($('<a>').attr('href', youtubeSearch + videoId));
+        img.wrap($('<a>').attr('href', youtubeSearch + videoId));
+        //show the list item on the page
+        $(ul).append(li);
     });
 };
